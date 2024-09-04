0f8c5d97fccea1e3e7808f737024fbf8573efaba
KristiyanYanakiev <yankris_92@yahoo.com>
2024-09-04 17:14:22 +0300

    added optimization

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,12 +1,13 @@
 from collections import deque
 from typing import Dict, Deque, List
 
+from check_for_win_mixin import CheckWinMixin
 from player import Player
 from player_one import PlayerOne
 from player_two import PlayerTwo
 
 
-class TicTacToe:
+class TicTacToe(CheckWinMixin):
     SIZE_OF_BOARD = 3
 
     starting_indecies_mapper: Dict[str, tuple] = {
@@ -71,64 +72,6 @@ class TicTacToe:
 
     def check_for_draw(self) -> bool:
         if self.turns == self.SIZE_OF_BOARD ** 2:
-            return True
-        return False
-
-    def check_for_first_horizontal_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["first_horizontal_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][c] == symbol for c in range(self.SIZE_OF_BOARD)])
-
-    def check_for_second_horizontal_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["second_horizontal_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][c] == symbol for c in range(self.SIZE_OF_BOARD)])
-
-    def check_for_third_horizontal_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["third_horizontal_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][c] == symbol for c in range(self.SIZE_OF_BOARD)])
-
-    def check_for_first_vertical_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["first_vertical_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][c] == symbol for r in range(self.SIZE_OF_BOARD)])
-
-    def check_for_second_vertical_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["second_vertical_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][c] == symbol for r in range(self.SIZE_OF_BOARD)])
-
-    def check_for_third_vertical_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["third_vertical_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][c] == symbol for r in range(self.SIZE_OF_BOARD)])
-
-    def check_for_first_diagonal_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["first_diagonal_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][r] == symbol for r in range(self.SIZE_OF_BOARD)])
-
-    def check_for_second_diagonal_win(self) -> bool:
-        r, c = self.starting_indecies_mapper["second_diagonal_win"]
-        symbol = self.board[r][c]
-
-        return all([self.board[r][r] == symbol for r in range(self.SIZE_OF_BOARD - 1, -1, -1)])
-
-    def check_win(self) -> bool:
-        possible_wins = [self.check_for_first_horizontal_win(), self.check_for_second_horizontal_win(),
-                         self.check_for_third_horizontal_win(), self.check_for_first_vertical_win(),
-                         self.check_for_second_vertical_win(), self.check_for_third_vertical_win(),
-                         self.check_for_first_diagonal_win(), self.check_for_second_diagonal_win()]
-
-        if any(possible_wins):
             return True
         return False
 
